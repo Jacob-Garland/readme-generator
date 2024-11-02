de36635a467045505b0ea3b51b3675d60f516076
Jacob Garland <352cjg@gmail.com>
2024-11-02 03:24:48 -0400

    progress on generator code

--- index.js
+++ index.js
@@ -9,21 +9,25 @@ const questions = [
     {type: 'input', name: description, message: "Give a brief description. i.e.: motivation, purpose"},
     {type: 'input', name: install, message: "Give a brief explanation of installation instructions."},
     {type: 'input', name: authors, message: "Who are the authors? Should include yourself, sources of code, collaborators, resources, etc."},
-    {type: 'input', name: license, message: "What license will your project need?"},
-    {type: 'input', name: restingPlace, message: "What's the directory path you're saving to?"},
+    {type: 'confirm', name: confirmLicense, message: "would you like to add a license to your project README?", default: true},
+    {type: 'list', name: license, message: "What license will your project need?", choices: ['agpl-3.0', 'gpl-3.0', 'lgpl-3.0', 'mpl-2.0', 'apache-2.0', 'mit', 'bsl-1.0', 'unlicense']},
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(data) {
-    fs.writeFile('README.md', data, (err) => {if (err) return err});
-    console.log('Successfully created a readme!');
+    fs.writeFile(`README.md`, data, (err) => {if (err) throw err});
+    console.log('Successfully generated a README.md!');
 };
 
 // TODO: Create a function to initialize app
 function init() {
     console.log('Program has started...');
     inquirer.prompt(questions)
-    .then((answers) => {generateMarkdown(answers);});
+    .then((answers) => {
+        generateMarkdown(answers);
+        writeToFile()
+    });
+    
 }
 
 // Function call to initialize app
--- readme.md
+++ readme.md
@@ -6,11 +6,29 @@ This is a generator that, using the command line, creates high quality readme fi
 The motivation behind this project was to save time coding by automating a trivial task in future projects.
 By using this readme generator, you can create a framework for a readme.md in your command line with a few simple questions answered.
 
+## Table of Contents
+
+- [Screenshots](#screenshots)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
 ## Screenshots
 
 ## Installation
 
 ## Usage
 
+## Credits
+
+- Jacob Garland
+- [shields.io](https://shields.io/)
+- [Pro Readme Guide](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+- [Choose A License](https://choosealicense.com/)
+
 ## License
 
+![Static Badge](https://img.shields.io/badge/License-ISC-blue)
+
+This project is licensed under the ISC License. For more details, refer to the License.md file.
