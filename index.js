d342afe4ebfe73b3273c52c3fe952adbcfbe2eaa
Jacob Garland <352cjg@gmail.com>
2024-11-04 19:03:56 -0500

    edit..

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 import fs from 'fs';
 import inquirer from 'inquirer';
-import generateMarkdown from './utils/generateMarkdown';
+import generateMarkdown from './utils/generateMarkdown.js';
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -9,7 +9,7 @@ const questions = [
     {type: 'input', name: description, message: "Give a brief description. i.e.: motivation, purpose"},
     {type: 'input', name: install, message: "Give a brief explanation of installation instructions."},
     {type: 'input', name: authors, message: "Who are the authors? Should include yourself, sources of code, collaborators, resources, etc."},
-    {type: 'confirm', name: confirmLicense, message: "would you like to add a license to your project README?", default: true},
+    {type: 'confirm', name: confirmLicense, message: "Would you like to add a license to your project README?", default: true},
     {type: 'list', name: license, message: "What license will your project need?", choices: ['agpl-3.0', 'gpl-3.0', 'lgpl-3.0', 'mpl-2.0', 'apache-2.0', 'mit', 'bsl-1.0', 'unlicense']},
 ];
 
