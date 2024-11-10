6e5e46970f55a5f31f3fca11b0b6049c4de01985
Jacob Garland <352cjg@gmail.com>
2024-11-09 23:25:16 -0500

    some final edits, not done but close

--- index.js
+++ index.js
@@ -5,29 +5,60 @@ import generateMarkdown from './utils/generateMarkdown.js';
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {type: 'input', name: title, message: "What is your project title?"},
-    {type: 'input', name: description, message: "Give a brief description. i.e.: motivation, purpose"},
-    {type: 'input', name: install, message: "Give a brief explanation of installation instructions."},
-    {type: 'input', name: authors, message: "Who are the authors? Should include yourself, sources of code, collaborators, resources, etc."},
-    {type: 'confirm', name: confirmLicense, message: "Would you like to add a license to your project README?", default: true},
-    {type: 'list', name: license, message: "What license will your project need?", choices: ['agpl-3.0', 'gpl-3.0', 'lgpl-3.0', 'mpl-2.0', 'apache-2.0', 'mit', 'bsl-1.0', 'unlicense']},
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Give a brief description. i.e.: motivation, purpose',
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Give a brief explanation of installation instructions.',
+    },
+    {
+        type: 'input',
+        name: 'dependancies',
+        message: 'Give a brief description of dependancies, if applicable.'
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Who are the contributors?',
+    },
+    {
+        type: 'confirm',
+        name: 'confirmLicense',
+        message: 'Would you like to add a license to your project README?',
+        default: true,
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license will your project need?',
+        choices: ['agpl-3.0', 'gpl-3.0', 'lgpl-3.0', 'mpl-2.0', 'apache-2.0', 'mit', 'bsl-1.0', 'unlicense'],
+    },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(data) {
-    fs.writeFile(`README.md`, data, (err) => {if (err) throw err});
-    console.log('Successfully generated a README.md!');
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), (err) => {if (err) throw err});
+    console.log('Successfully created a readme file! Thank you for using the Readme Generator.');
+    console.log('Check the finalProduct folder of this program for your readme.md');
+    console.log('Program has ended...')
 };
 
 // TODO: Create a function to initialize app
 function init() {
     console.log('Program has started...');
     inquirer.prompt(questions)
-    .then((answers) => {
-        generateMarkdown(answers);
-        writeToFile()
+    .then((data) => {
+        writeToFile("./finalProduct/readme.md", data);
     });
-    
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if (confirmLicnese === true) {
+function renderLicenseBadge(confirmLicense, license) {
+  if (confirmLicense === true) {
     return `![Static Badge](https://img.shields.io/badge/License-${license}-blue)`
   } else {
     return '';
@@ -10,7 +10,7 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
+function renderLicenseLink(confirmLicense, license) {
   if (confirmLicense === true) {
     return `[${license}](https://choosealicense.com/licenses/${license}/)`;
   } else {
@@ -20,22 +20,23 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {
-  switch (license) {
-    case 'mit':
-      return `## License\n\nThis project is licensed under the ${renderLicenseLink(license)} License.\n\n## Badges\n\n${renderLicenseBadge(license)}`;
-  }
+function renderLicenseSection(confirmLicense, license) {
+  if (confirmLicense === true) {
+    return `## License\n\nThis project is licensed under the ${renderLicenseLink(license)} License.\n\n## Badges\n\n${renderLicenseBadge(license)}`;
+  } else {
+    return '';
+  };
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-    ## Description
+    # Description
 
     ${data.description}
 
-    ## Table Of Contents
+    # Table Of Contents
 
     - [Installation](#installation)
     - [Usage](#usage)
@@ -44,27 +45,30 @@ function generateMarkdown(data) {
     - [License](#license)
     - [Badges](#badges)
 
-    ## Installation
+    # Installation
+    
+    ## Dependancies Required
 
-    ### Dependancies
+    ${data.dependancies}
 
-    ### Installing
+    ## Installing this project
 
     ${data.install}
-
-    ## Usage
-
-    In this section, you would write a description and instructions of how to use your application.
-    To add a screenshot, create an assets/images folder in your repository and upload your screenshot to it. 
-    Then, using the relative filepath, add it to your README. Don't forget to use Markdown Language!
-
-    ## Credits
-
-    ${data.authors}
-
-    ## How To Contribute
     
-
+    # Usage
+    
+    In this section, you should write a description of instructions on how to use your application.
+    To add a screenshot, create an assets/images folder in your repository and place your screenshot in it. 
+    Then, using the relative filepath, add it to your README. Don't forget to use Markdown Language!
+    
+    # Contributors
+    
+    ${data.contributors}
+    
+    # How To Contribute
+    
+    In this section, you want to give instructions on how others can contribute to your project.
+    
     ${renderLicenseSection(license)}
 `;
 }
